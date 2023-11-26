f5afce514b555fc5bfd284b4b721bc33dffde0fa
T-redd01 <tyrilreddy1@gmail.com>
2023-11-26 06:58:34 -0800

    checks fail

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # ensuring file exists
 
 file { '/tmp/school':
-  ensure => file,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0744',
-  cotent => 'I love Puppet',
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
 }
